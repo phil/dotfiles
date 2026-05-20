06c1bbf9270c7b94951840e39544c195bfc47ddd
Phil Balchin <phil@maniacalrobot.co.uk>
2026-05-20 14:02:42 +0100

    maybe got treesitter working in nvim

--- nvim/.config/nvim/lua/packs/treesitter.lua
+++ nvim/.config/nvim/lua/packs/treesitter.lua
@@ -1,42 +1,82 @@
 vim.pack.add({
-    "https://github.com/nvim-treesitter/nvim-treesitter"
+    {src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main"}
 })
+--
+-- require("nvim-treesitter").setup({
+--     ensure_installed = {
+--         'bash',
+--         'c',
+--         'css',
+--         'diff',
+--         'graphql',
+--         'html',
+--         'javascript',
+--         'typescript',
+--         'json',
+--         'ruby',
+--         'lua',
+--         'luadoc',
+--         'markdown',
+--         'markdown_inline',
+--         'query',
+--         'vim',
+--         'vimdoc'
+--     },
+--     auto_install = true,
+--     highlight = {
+--         enable = true,
+--         additional_vim_regex_highlighting = {
+--             'markdown',
+--             'ruby'
+--         }
+--     },
+--     indent = {
+--         enable = true,
+--         disable = {
+--             'ruby'
+--         }
+--     },
+--     fold = {
+--         enable = true
+--     }
+-- })
 
-require("nvim-treesitter").setup({
-    ensure_installed = {
-        'bash',
-        'c',
-        'css',
-        'diff',
-        'graphql',
-        'html',
-        'javascript',
-        'typescript',
-        'json',
-        'ruby',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc'
-    },
-    auto_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = {
-            'markdown',
-            'ruby'
-        }
-    },
-    indent = {
-        enable = true,
-        disable = {
-            'ruby'
-        }
-    },
-    fold = {
-        enable = true
-    }
+-- require("treesitter")
+local treesitter = require("nvim-treesitter")
+
+local ensure_installed = {
+    'bash',
+    'c',
+    'css',
+    'diff',
+    'graphql',
+    'html',
+    'javascript',
+    'typescript',
+    'json',
+    'ruby',
+    'lua',
+    'luadoc',
+    'markdown',
+    'markdown_inline',
+    'query',
+    'vim',
+    'vimdoc'
+}
+
+treesitter.install(ensure_installed)
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function(args)
+        local buf = args.buf
+        local ft = vim.bo[buf].filetype
+        local lang = vim.treesitter.language.get_lang(ft)
+        if not lang then return
+        end
+        local ok_add = pcall(vim.treesitter.language.add, lang)
+        if not ok_add then return
+        end
+        pcall(vim.treesitter.start, buf, lang)
+    end,
 })
